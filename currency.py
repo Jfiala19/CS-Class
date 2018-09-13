4450507fe88b64ccb2b30180c566b71be95d37bd
Jack Fiala <jfiala19@choate.edu>
2018-09-13 14:19:54 -0400

    classwork with random function

---
+++ currency.py
@@ -0,0 +1,14 @@
+# Currency Converter
+# Dollars to Yen
+import sys
+dollars = sys.argv[1]
+currency = sys.argv[2]
+dollarFloat = float(dollars)
+Output = dollarFloat * 111.61
+print(currency+':',Output)
+
+
+
+
+
+
