17515f2d8bb89fe16ccbb3dfa119111e43240057
Jack Fiala <jfiala19@choate.edu>
2018-09-27 22:15:43 -0400

    hi

--- ChallangeQuestions/Question2.py
+++ ChallangeQuestions/Question2.py
@@ -1,4 +1,4 @@
-#https://stackoverflow.com/questions/976882/shuffling-a-list-of-objects
+#9/27/18 Question2 https://stackoverflow.com/questions/976882/shuffling-a-list-of-objects
 import random
 c = [x for x in range(1, 101)]
 random.shuffle(c)
--- Question1.py
+++
@@ -1,9 +0,0 @@
-#9/27/18, Question1, https://wiki.python.org/moin/HowTo/Sorting
-import random
-x =[]
-while len(x) < 10:
-	y = random.randint(0, 100)
-	if y%3 != 0:
-		x.append(y)
-x.sort()
-print(x)
