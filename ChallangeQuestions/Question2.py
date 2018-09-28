637eb40879b9d71f37a30bfb4d2acff08a74d547
Jack Fiala <jfiala19@choate.edu>
2018-09-27 22:11:27 -0400

    HW Solutions

---
+++ ChallangeQuestions/Question2.py
@@ -0,0 +1,5 @@
+#https://stackoverflow.com/questions/976882/shuffling-a-list-of-objects
+import random
+c = [x for x in range(1, 101)]
+random.shuffle(c)
+print("Your list has been shuffled\n\n", *c)
