5d101ad20cee3f036dd6197ae119cdce6f078947
Jack Fiala <jfiala19@choate.edu>
2018-10-26 10:07:55 -0400

    HW

--- Bankaccount.py
+++ Bankaccount.py
@@ -1,43 +1,44 @@
 class Account: 
 
-	def __init__(Name, Pin, Balance, OC, SQ, SA): 
-		self.Name = Name
-		self.Pin = Pin
-		self.Balance = Balance
+	def __init__(self, name, pin, balance, OC, SQ, SA): 
+		self.name = name
+		self.pin = pin
+		self.balance = balance
 		self.OC = OC
 		self.SQ = SQ
 		self.SA = SA
 	#Methods (A function within a class)
-	def withdraw(amount):
-		if self.balance > amount:
-			self.balance -= amount 
-			status = "You withdrew $" +str(amount) " and now have a balance of $" +str(self.balance) 
+	def withdraw(self, amount):
+		if self.balance >= int(amount):
+			self.balance -= int(amount) 
+			status = "\nYou withdrew $" +amount+  " and now have a balance of $" +str(self.balance) +"\n"
 		else:
-			status = "You balance is $" +self.balance " . You can't withdraw this much"
+			status = "\nYou balance is $" +str(self.balance) +". You can't withdraw this much \n"
 		return status
-	def deposit(amount):
-		self.balance += amount
-		status = "Your balance is now $" +self.balance
+	def deposit(self, amount):
+		self.balance +=int(amount)
+		status = "\nYour balance is now $" +str(self.balance) +"\n"
 		return status
 
 
 
 def accountmanagement(account):
-While True:	
-	action = input("What would you like to do: \n\n1) View Balance\n2)Deposit\n3)Withdraw\n4)Exit")
-	if action == 1:
-		print(account.Balance)
-	if action == 2:
-		amount = input("How much would you like to Deposit?")
-		print(account.deposit(amount))
-	if action == 3:	
-		amount = input("How much would you like to Withdraw?")
-		print(account.withdraw(amount))
-	if action == 4:
-		break()
+
+	while True:	
+		action = input("\nWhat would you like to do: \n\n1)View Balance\n2)Deposit\n3)Withdraw\n4)Exit\n>>")
+		if action == "1":
+			print("\nYour Balance: $"+str(account.balance)+"\n")
+		if action == "2":
+			amount = input("\nHow much would you like to Deposit?\n>>")
+			print("\n"+str(account.deposit(amount))+"\n")
+		if action == "3":	
+			amount = input("\nHow much would you like to Withdraw?\n>>")
+			print("\n"+str(account.withdraw(amount))+"\n")
+		if action == "4":
+			break
 
 
-333444a = Account(Jack, 1234, 100, 1, "What was the name of your first pet?", "Cali")
+a44a = Account("Jack", "1234", 100, 1, "What was the name of your first pet?", "Cali")
 
 
 
@@ -46,17 +47,17 @@ While True:
 while True:
 	try1 = input("Enter your account number: ")
 	try2 = input("Enter your Pin Number: ")
-	if try2 == 333444a.pin:
-		print("Hello "+333444a.Name)
-		accountmanagement(333444a)
+	if try2 == a44a.pin:
+		print("\nHello "+a44a.name)
+		accountmanagement(a44a)
 	else:
-		try3 = input("Incorrect.\n Eneter your Pin Number: ")
-		if try3 == 333444a.pin:
-			print("Hello "+333444a.Name)
-			accountmanagement(333444a)
+		try3 = input("\nIncorrect.\n Eneter your Pin Number: ")
+		if try3 == a44a.pin:
+			print("\nHello "+a44a.name)
+			accountmanagement(a44a)
 		else:
-			try4 = input("Incorrect.\n " +333444a.SQ)
-			if try4 == 333444a.SA:
-				print("Hello "+333444a.Name)
-				accountmanagement(333444a)
+			try4 = input("\nIncorrect.\n " +a44a.SQ)
+			if try4 == a44a.SA:
+				print("\nHello "+a44a.name +"\n")
+				accountmanagement(a44a)
 
